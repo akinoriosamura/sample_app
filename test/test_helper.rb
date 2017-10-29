4fdd4ff2abf1def31bc189e6dd76bd6110601508
akinoriosamura <osamura.akinori@gmail.com>
2017-10-29 17:43:37 +0900

    Finish log in/log out

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,13 @@ require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
+ENV['RAILS_ENV'] ||= 'test'
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  def is_logged_in?
+      !session[:user_id].nil?
+  end
 
-  # Add more helper methods to be used by all tests here...
 end
