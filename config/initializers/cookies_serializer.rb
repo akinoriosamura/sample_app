4ffb647ca3669781a6fc1481f7daa2d6091a75dd
akinoriosamura <osamura.akinori@gmail.com>
2017-10-31 21:37:11 +0900

    add user microposts

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -2,4 +2,16 @@
 
 # Specify a serializer for the signed and encrypted cookie jars.
 # Valid options are :json, :marshal, and :hybrid.
-Rails.application.config.action_dispatch.cookies_serializer = :json
+# Rails.application.config.action_dispatch.cookies_serializer = :json
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
+end
